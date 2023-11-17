59a7a75b6896a7bd5c055230405920bf87bd9aa9
Antonio Vilchez <antoniovmonge@gmail.com>
2023-11-17 18:38:12 +0100

    Add phone number and client status fields to User model, update code formatting

--- c_ride/users/models.py
+++ c_ride/users/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, EmailField
+from django.core.validators import RegexValidator
+from django.db.models import BooleanField, CharField, EmailField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from c_ride.users.managers import UserManager
+from c_ride.utils.models import CRideModel
 
 
-class User(AbstractUser):
+class User(CRideModel, AbstractUser):
     """
     Default custom user model for c_ride.
     If adding fields that need to be filled at user signup,
@@ -20,8 +22,31 @@ class User(AbstractUser):
     email = EmailField(_("email address"), unique=True)
     username = None  # type: ignore
 
+    phone_regex = RegexValidator(
+        regex=r"^\+?1?\d{9,15}$",
+        message=_(
+            "Phone number must be entered in the format: '+999999999'."
+            "Up to 15 digits allowed."
+        ),
+    )
+    phone_number = CharField(
+        _("Phone number"), validators=[phone_regex], blank=True, max_length=17
+    )
+    phone_number_verified = BooleanField(
+        _("Phone number verified"), default=False
+    )
+
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    is_client = BooleanField(
+        "client status",
+        default=True,
+        help_text=(
+            "Help easily distinguish users and perform queries."
+            "Clients are the main type of user."
+        ),
+    )
 
     objects = UserManager()
 
@@ -33,3 +58,11 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"pk": self.id})
+
+    def __str__(self) -> str:
+        """Return username."""
+        return self.email
+
+    def get_short_name(self) -> str:
+        """Return username."""
+        return self.email

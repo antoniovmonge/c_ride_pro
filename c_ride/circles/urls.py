16f702651a697bd2647f1796ad6f6a359803a036
Antonio Vilchez <antoniovmonge@gmail.com>
2023-11-20 19:53:51 +0100

    Add create_circle view to circles.urls.py

--- c_ride/circles/urls.py
+++ c_ride/circles/urls.py
@@ -3,10 +3,11 @@
 # Django
 from django.urls import path
 
-from c_ride.circles.views import list_circles
+from c_ride.circles.views import create_circle, list_circles
 
 app_name = "circles"
 
 urlpatterns = [
     path("", list_circles, name="list_circles"),
+    path("create/", create_circle, name="create_circle"),
 ]
--- c_ride/circles/views.py
+++ c_ride/circles/views.py
@@ -1,10 +1,14 @@
 """Circle views."""
 
-from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from c_ride.circles.models import Circle
 
 
+@api_view(["GET"])
+@permission_classes([AllowAny])
 def list_circles(request):
     """List existing circles."""
     public_circles = Circle.objects.filter(is_public=True)
@@ -21,4 +25,28 @@ def list_circles(request):
                 "members_limit": circle.members_limit,
             }
         )
-    return JsonResponse(data, safe=False)
+    return Response(data)
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def create_circle(request):
+    name = request.data["name"]
+    slug_name = request.data["slug_name"]
+    about = request.data.get("about", "")
+
+    circle = Circle.objects.create(
+        name=name,
+        slug_name=slug_name,
+        about=about,
+    )
+    data = {
+        "name": circle.name,
+        "slug_name": circle.slug_name,
+        "rides_offered": circle.rides_offered,
+        "rides_taken": circle.rides_taken,
+        "verified": circle.verified,
+        "is_limited": circle.is_limited,
+        "members_limit": circle.members_limit,
+    }
+    return Response(data)

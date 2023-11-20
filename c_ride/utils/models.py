8bc88d54dd0b25f72cbf9b617a15ded117ed4fd0
Antonio Vilchez <antoniovmonge@gmail.com>
2023-11-20 10:08:45 +0100

    fixed default=timezone.now CRideModel

--- c_ride/utils/models.py
+++ c_ride/utils/models.py
@@ -2,6 +2,7 @@
 
 # Django
 from django.db import models
+from django.utils import timezone
 
 
 class CRideModel(models.Model):
@@ -16,12 +17,14 @@ class CRideModel(models.Model):
 
     created = models.DateTimeField(
         "created at",
-        auto_now_add=True,
+        # auto_now_add=True,
+        default=timezone.now,
         help_text="Date time on which the object was created.",
     )
     modified = models.DateTimeField(
         "modified at",
-        auto_now_add=True,
+        # auto_now_add=True,
+        default=timezone.now,
         help_text="Date time on which the object was last modified.",
     )
 

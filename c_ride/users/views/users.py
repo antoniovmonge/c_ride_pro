09ae112cf7237e379f1089946b3cd478b9d50aa0
Antonio Vilchez <antoniovmonge@gmail.com>
2023-11-26 16:15:36 +0100

    Refactor user URLs and views

--- c_ride/users/views/users.py
+++ c_ride/users/views/users.py
@@ -6,9 +6,9 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import DetailView, RedirectView, UpdateView
 
 # Django REST Framework
-from rest_framework import status
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 # Serializers
 from c_ride.users.serializers import (
@@ -58,11 +58,15 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 user_redirect_view = UserRedirectView.as_view()
 
 
-class UserLoginAPIView(APIView):
-    """User login API view."""
+class UserViewSet(viewsets.GenericViewSet):
+    """User view set.
 
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    Handle sign up, login and account verification.
+    """
+
+    @action(detail=False, methods=["post"])
+    def login(self, request):
+        """User sign in."""
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user, token = serializer.save()
@@ -72,24 +76,18 @@ class UserLoginAPIView(APIView):
         }
         return Response(data, status=status.HTTP_201_CREATED)
 
-
-class UserSignUpAPIView(APIView):
-    """User sign up API view."""
-
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    @action(detail=False, methods=["post"])
+    def signup(self, request):
+        """User sign up."""
         serializer = UserSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         data = UserModelSerializer(user).data
         return Response(data, status=status.HTTP_201_CREATED)
 
-
-class AccountVerificationAPIView(APIView):
-    """Account verification API view."""
-
-    def post(self, request, *args, **kwargs):
-        """Handle HTTP POST request."""
+    @action(detail=False, methods=["post"])
+    def verify(self, request):
+        """Account verification."""
         serializer = AccountVerificationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
